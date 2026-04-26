3fc82f733d77e7f3b08879fd14c8e2ef058e8a40
codebykashish <kashdhami74@gmail.com>
2026-04-26 15:07:45 +0545

    delete

--- firebase_config.py
+++ firebase_config.py
@@ -1,22 +1,31 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-
-import json
 import os
-cred = credentials.Certificate(json.loads(os.getenv('FIREBASE_SERVICE_ACCOUNT')))
-
-# This runs only once when app starts
-# It connects to your Firebase project
+import json
 
 _app = None
 
 def initialize_firebase():
     global _app
     if _app is None:
-        cred = credentials.Certificate("serviceAccountKey.json")
+        cred = None
+        
+        # Local FIRST
+        if os.path.exists("serviceAccountKey.json"):
+            cred = credentials.Certificate("serviceAccountKey.json")
+            print("✅ Local serviceAccountKey.json")
+        
+        # Railway SECOND
+        elif os.getenv("FIREBASE_SERVICE_ACCOUNT"):
+            service_account = json.loads(os.getenv("FIREBASE_SERVICE_ACCOUNT"))
+            cred = credentials.Certificate(service_account)
+            print("✅ Railway env")
+        
+        if cred is None:
+            raise Exception("Missing serviceAccountKey.json or FIREBASE_SERVICE_ACCOUNT env")
+        
         _app = firebase_admin.initialize_app(cred)
-        print("✅ Firebase initialized successfully")
+        print("✅ Firebase initialized")
 
 def get_firestore():
-    # Returns Firestore database client
     return firestore.client()
